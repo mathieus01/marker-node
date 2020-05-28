2ce3fd681bec8b2c26954d806b97ccb1ee0a3d96
Matheus Nunes <mathieusnunes@gmail.com>
2020-05-28 08:43:26 -0300

    removido youch

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,9 +1,8 @@
-"use strict";
+'use strict';
 
-const Youch = use("Youch");
-const Env = use("Env");
+const Env = use('Env');
 
-const BaseExceptionHandler = use("BaseExceptionHandler");
+const BaseExceptionHandler = use('BaseExceptionHandler');
 
 /**
  * This class handles all exceptions thrown during
@@ -13,15 +12,15 @@ const BaseExceptionHandler = use("BaseExceptionHandler");
  */
 class ExceptionHandler extends BaseExceptionHandler {
   async handle(error, { request, response }) {
-    if (error.name === "ValidationException") {
+    if (error.name === 'ValidationException') {
       return response.status(error.status).send(error.messages);
     }
-    if (Env.get("NODE_ENV") === "development") {
-      const youch = new Youch(error, request.request);
-      const errorJson = await youch.toJSON();
+    // if (Env.get("NODE_ENV") === "development") {
+    //   const youch = new Youch(error, request.request);
+    //   const errorJson = await youch.toJSON();
 
-      return response.status(error.status).send(errorJson);
-    }
+    //   return response.status(error.status).send(errorJson);
+    // }
 
     return response.send(error.status);
   }
